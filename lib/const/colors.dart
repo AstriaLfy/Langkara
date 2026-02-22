0a90d302812689833f92c596d021ae3f1d51c360
AstriaLfy <pravdaairen@gmail.com>
2026-02-23 01:44:46 +0700

    Connect Supabase

---
+++ lib/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class colors{
+    static const Color violet = Color(0xF3E3E4);
+    static const Color blue = Color(0x283E57);
+    static const Color yellow = Color(0xE4C6AF);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:langkara/Pages/on_boarding_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: "https://exdtkmfhqtgewykwvgmc.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZHRrbWZocXRnZXd5a3d2Z21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDAzNTMsImV4cCI6MjA4NzMxNjM1M30.pMTAsc-bA0pgD8im63JSTwu50mAkOW9DH-eNzPklgmc",
+  );
+
   runApp(const MyApp());
 }
 
@@ -12,32 +22,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-      ),
+        textTheme: GoogleFonts.sourceSans3TextTheme(Theme.of(context).textTheme,),),
+      home: OnBoardingPage(),
     );
   }
 }
