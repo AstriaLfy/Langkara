273e332cb8915bfdfc0ab75d603a57d303ffae9c
AstriaLfy <pravdaairen@gmail.com>
2026-02-28 13:50:55 +0700

    config: setting bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:langkara/Pages/on_boarding_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:langkara/Bloc/Auth/auth_bloc.dart';
+import 'package:langkara/Pages/welcome_page.dart';
+import 'package:langkara/Repository/auth_repository.dart';
+import 'package:langkara/Services/auth_services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -11,19 +14,25 @@ void main() async {
     anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4ZHRrbWZocXRnZXd5a3d2Z21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDAzNTMsImV4cCI6MjA4NzMxNjM1M30.pMTAsc-bA0pgD8im63JSTwu50mAkOW9DH-eNzPklgmc",
   );
 
-  runApp(const MyApp());
+  final authService = AuthService();
+  final authRepository = AuthRepository(authService);
+
+  runApp(MyApp(authRepository: authRepository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AuthRepository authRepository;
+
+  const MyApp({super.key, required this.authRepository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.sourceSans3TextTheme(Theme.of(context).textTheme,),),
-      home: OnBoardingPage(),
+    return BlocProvider<AuthBloc>(
+      create: (_) => AuthBloc(authRepository),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomePage(),
+      ),
     );
   }
 }
