9ac1e90138d1a71d88b68fc07cc0bab41686fe4b
andrewwilliam13 <andrewwilliam@student.ub.ac.id>
2026-02-28 14:34:49 +0700

    onboarding page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:langkara/pages/onBoardingPages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,34 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 90, 194, 21)),
       ),
-      home: const MyHomePage(title: 'test ganti'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-      ),
+      home: const OnboardingPage()
     );
   }
 }
