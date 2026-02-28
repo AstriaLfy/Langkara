e3e63c0d06c3281af95054e2fab266f99d98fdce
AstriaLfy <pravdaairen@gmail.com>
2026-02-28 13:48:44 +0700

    chore: update

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class colors{
     static const Color violet = Color(0xF3E3E4);
-    static const Color blue = Color(0x283E57);
+    static const Color blue = Color(0xFF1A2A4F);
     static const Color yellow = Color(0xE4C6AF);
 }
